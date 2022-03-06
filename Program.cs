bb3262a568b2d201562245a391fe40440a885414
chandana03 <chandanareshma01@gmail.com>
2022-03-06 23:34:11 +0530

    linelength

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace linecomparisonoops
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Line comparison");
+
+
+            linelength obj = new linelength();
+            obj.lengthofline();
+
+            //lineequity obj = new lineequity();
+            //obj.line_equity();
+
+            //equitycomparison obj = new equitycomparison();
+            //obj.comparingline();
+        }
+
+    }
+}
